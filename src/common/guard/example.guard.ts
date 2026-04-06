2a05d13f24edf6a4373e5797cca71e4448457f50
Huy Dang <songhuy304@gmail.com>
2026-04-06 22:43:45 +0700

    feat: validate user guard

--- src/common/guard/example.guard.ts
+++ src/common/guard/example.guard.ts
@@ -1,17 +1,22 @@
 import {
-  CanActivate,
   ExecutionContext,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PUBLIC_ROUTE_KEY } from './constants/guard.constant';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class Exemple implements CanActivate {
-  constructor(private reflector: Reflector) {}
+export class Exemple extends AuthGuard('jwt-access') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
 
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(
       PUBLIC_ROUTE_KEY,
       [context.getHandler(), context.getClass()],
@@ -19,12 +24,6 @@ export class Exemple implements CanActivate {
 
     if (isPublic) return true;
 
-    const request = context.switchToHttp().getRequest<Request>();
-    const token = request.headers['authorization'] as string;
-    if (!token) {
-      throw new UnauthorizedException('Token not found');
-    }
-
-    return true;
+    return super.canActivate(context);
   }
 }
